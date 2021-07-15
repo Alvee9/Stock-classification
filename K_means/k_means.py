8c85d2320f6664cbe58b69b33161d1516932b22e
Abdullah Al Imam <abdullah.alvee@gmail.com>
2021-07-15 13:00:29 +0100

    Add n-days average option for trendlines

--- K_means/k_means.py
+++ K_means/k_means.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-
+import numpy as np
 
 def read_company_names():
     companies = []
@@ -12,7 +11,8 @@ def read_company_names():
 
 companies = read_company_names()
 
-def get_closing_prices(companies):
+
+def get_closing_prices(companies, number_of_companies):
     data = []
     for idx, company in enumerate(companies):
         # df = pd.read_csv('Dataset/Comp_time_series/{}.csv'.format(company))
@@ -23,11 +23,12 @@ def get_closing_prices(companies):
             data.append([idx] + list(df['Close']))
         except FileNotFoundError:
             print(company, 'Not found')
-        if len(data) == 100:
+        if len(data) == number_of_companies:
             break
     return data
 
-def calc_price_changes(companies):
+
+def calc_price_changes(companies, number_of_companies):
     data = []
     for idx, company in enumerate(companies):
         try:
@@ -42,42 +43,55 @@ def calc_price_changes(companies):
             price_changes[i] = (df['Close'][i] - df['Close'][i - 1]) / df['Close'][i - 1] * 100
         data.append(price_changes)
 
-        if len(data) == 100:
+        if len(data) == number_of_companies:
             break
     return data
 
-data = calc_price_changes(companies)
 
+def average_ndays(data, n):
+    new_data = []
+    for trendline in data:
+        sum = 0.0
+        new_trendline = [trendline[0]]
+        for i in range(1, len(trendline)):
+            sum += trendline[i]
+            divisor = i
+            if i > n:
+                divisor = n
+                sum -= trendline[i - n]
+            new_trendline.append(sum / divisor)
+        new_data.append(new_trendline)
+    return new_data
+
+
+# data = get_closing_prices(companies, 50)
+data = calc_price_changes(companies, 50)
+data = np.array(data)[:, 0:60]
 print(len(data))
+data = average_ndays(data, 7)
 
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.cluster import KMeans
+def clustering(number_of_clusters):
+    from matplotlib import pyplot as plt
+    from sklearn.cluster import KMeans
 
-kmeans = KMeans(n_clusters=10, random_state=0, n_init=50).fit(np.array(data)[:, 1:])
+    kmeans = KMeans(n_clusters=number_of_clusters, random_state=0, n_init=50).fit(np.array(data)[:, 1:])
 
-# Get the cluster centroids
-print(kmeans.cluster_centers_)
-    
-# Get the cluster labels
-print(kmeans.labels_)
-print(len(kmeans.labels_))
+    # Get the cluster centroids
+    print(kmeans.cluster_centers_)
+        
+    # Get the cluster labels
+    print(kmeans.labels_)
+    print(len(kmeans.labels_))
 
-# Plotting the cluster centers and the data points on a 2D plane
-# plt.scatter(data[:][0], data[:][1])
-    
-# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x')
-    
-# plt.title('Data points and cluster centroids')
-# plt.show().
+    for k in range(0, number_of_clusters):
+        clusterSize = 0
+        for idx, r in enumerate(data):
+            if kmeans.labels_[idx] == k:
+                plt.plot(r[1:], label = companies[int(r[0])])
+                clusterSize += 1
+        plt.title('Cluster number {} size = {}'.format(k, clusterSize))
+        plt.legend()
+        plt.show()
 
-for k in range(1, 10):
-    clusterSize = 0
-    for idx, r in enumerate(data):
-        if kmeans.labels_[idx] == k:
-            plt.plot(r[1:], label = companies[r[0]])
-            clusterSize += 1
-    plt.title('Cluster number {} size = {}'.format(k, clusterSize))
-    plt.legend()
-    plt.show()
 
+clustering(10)
